0620d3124020e892f4811d4b0e089033a4016b7e
kulyaginv <kulyaginv@gmail.com>
2022-02-03 22:00:21 +0300

    MultiContext 20220203 2159

---
+++ src/main/java/com/pluralsight/conference/ctx2/Ctx2Config.java
@@ -0,0 +1,14 @@
+package com.pluralsight.conference.ctx2;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("com.pluralsight.conference.ctx2")
+@PropertySource(value="classpath:ctx2-prod.properties")
+@EnableAutoConfiguration
+public class Ctx2Config {
+    
+}
