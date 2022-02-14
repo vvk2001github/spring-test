32a5148698d31fdc2779b96307b55e8316f595ce
kulyaginv <kulyaginv@gmail.com>
2022-02-14 20:20:40 +0300

    Added Delete Route To Workout

--- src/main/java/com/pluralsight/conference/ctx1/controller/WorkoutController.java
+++ src/main/java/com/pluralsight/conference/ctx1/controller/WorkoutController.java
@@ -18,10 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -87,6 +84,23 @@ public class WorkoutController {
 
         workoutRepository.save(workout);
         redirectAttrs.addFlashAttribute("success", helper.getLocalizedMsg("wmessages.successAdded"));
+        return "redirect:/workouts/index";
+    }
+
+    @RequestMapping(value="/{id}/delete", method=RequestMethod.GET)
+    public String delete(@PathVariable Long id, RedirectAttributes redirectAttrs, Model model) {
+        Workout workout = workoutRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid workout Id:" + id));
+
+        User currentUser = userService.findFirstByUsername(model.getAttribute("principalName").toString());
+        if(!currentUser.getId().equals(workout.getExid().getUser().getId())) {
+            redirectAttrs.addFlashAttribute("errors", "You can not delete this workout");
+            return "redirect:/workouts/index";
+        }
+        workoutRepository.delete(workout);
+
+        redirectAttrs.addFlashAttribute("success", "Workout successfully deleted");
+
         return "redirect:/workouts/index";
     }
     
